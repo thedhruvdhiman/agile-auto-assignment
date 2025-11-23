66df965e7bb24f9305f6e1d21e7b5663a08a96a1
Dhruv <dhruv261dhiman@gmail.com>
2025-11-23 23:47:27 +0530

    chore: Disable artifact upload in daily news workflow and add `Accept`, `Accept-Language`, and `Referer` headers to HTTP requests.

--- src/index.js
+++ src/index.js
@@ -61,6 +61,10 @@ const fetchReddit = async (keyword) => {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        Referer: "https://www.google.com/",
       },
     });
     return response.data.data.children.map((item) => ({
